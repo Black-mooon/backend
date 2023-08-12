5a024ed662713cfba7919f01b8026999a4fa5702
Srijan Gupta <gupta.srijan94@gmail.com>
2023-08-12 22:55:01 +0530

    fix: classroom users

--- routes/classroom.route.ts
+++ routes/classroom.route.ts
@@ -7,16 +7,19 @@ import {
   getOneById,
 } from '../controller/classroom.controller'
 import { isAdmin, isFaculty, protect } from '../middlewares/auth.middleware'
-import { addUserToClassroom, getClassroomUser } from '../controller/user_classroom.controller'
+import {
+  addUserToClassroom,
+  getClassroomUser,
+} from '../controller/user_classroom.controller'
 
 const router = express.Router()
 
 router.get('/', getAll)
-router.get('/:id', getOneById)
 router.post('/', [protect, isFaculty], create)
 router.post('/update', [protect, isFaculty], update)
 router.post('/add-subject', [protect, isFaculty], addSubject)
 router.post('/add-user', [protect, isFaculty], addUserToClassroom)
 router.get('/get-users', [protect, isFaculty], getClassroomUser)
+router.get('/:id', getOneById)
 
 export default router
