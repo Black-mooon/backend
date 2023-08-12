ec966dfd25e156d401a1ebade90a9b62ee816bc3
Srijan Gupta <gupta.srijan94@gmail.com>
2023-08-12 13:18:06 +0530

    feat: user auth

---
+++ middlewares/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { IToken } from '../utils/token'
+import User, { IUser } from '../models/user.model'
+
+export const protect = (
+  req: Request & { user: jwt.JwtPayload },
+  res: Response,
+  next: NextFunction
+) => {
+  const bearer: string = req.headers.authorization
+
+  if (!bearer) {
+    res.status(401).json({ message: 'not authorized' })
+    return
+  }
+
+  const [, token] = bearer.split(' ')
+
+  if (!token) {
+    res.status(401).json({ message: 'not valid token' })
+    return
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user as IToken
+    next()
+  } catch (e) {
+    console.error(e)
+    res.status(401).json({ message: 'not valid token' })
+    return
+  }
+}
---
+++ utils/token.ts
@@ -0,0 +1,34 @@
+import { IUser } from '../models/user.model'
+import jwt from 'jsonwebtoken'
+
+export interface IToken {
+  id: string
+}
+
+export const createToken = (user: IUser): string => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET as jwt.Secret,
+    {
+      expiresIn: '1d',
+    }
+  )
+}
+
+export const verifyToken = async (
+  token: string
+): Promise<jwt.VerifyErrors | IToken> => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET as jwt.Secret,
+      (err: any, payload: any) => {
+        if (err) return reject(err)
+
+        resolve(payload as IToken)
+      }
+    )
+  })
+}
+
+export default { createToken, verifyToken }
